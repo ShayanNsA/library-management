85a6dbd03a4157bdbba2f94c5d7ebc6051b4378e
Shayan_Naseri <shayan.naseri.aghdam@gmail.com>
2026-08-29 23:23:28 +0330

    you can create a book. we can update this feature in next commits

--- library/urls.py
+++ library/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('books/', views.book_list, name='book-list'),
     path('', views.home, name='home'),
+    path('books/create/', views.book_create, name='book-create'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import Book
+from .forms import BookForm
+
+
 
 
 def book_list(request):
     books = Book.objects.all()
 
-    return render(request, 'library/book_list.html', {
-        'books': books
-    })
+    return render(request, 'library/book_list.html', {'books': books})
 
 def home(request):
     return render(request, 'library/home.html')
+
+
+def book_create(request):
+
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('book-list')
+
+    else:
+        form = BookForm()
+
+    return render(request, 'library/book_create.html', {
+        'form': form
+    })
