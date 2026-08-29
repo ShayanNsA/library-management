6566e438f09e8f9bfdebe4a45b0f035b4d8f5a8f
Shayan_Naseri <shayan.naseri.aghdam@gmail.com>
2026-08-29 18:01:30 +0330

    you can use home panel and you can see the all off books

--- library/views.py
+++ library/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Book
 
-# Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+
+    return render(request, 'library/book_list.html', {
+        'books': books
+    })
+
+def home(request):
+    return render(request, 'library/home.html')
