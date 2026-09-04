15280d6218221d6bb0ca39377105c6d23dce127f
Shayan_Naseri <shayan.naseri.aghdam@gmail.com>
2026-09-04 23:39:51 +0330

    create admin panel and upgrade models

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Author, Book, Category
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Category)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_authors', 'publish_date','pages', 'id', 'price' )
+
+    @admin.display(description='Authors')
+    def get_authors(self, obj):
+
+        return " ".join([author.first_name + " " + author.last_name for author in obj.authors.all()])
+
+
+
--- library/models.py
+++ library/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError # این رو ایمپورت میکنیم تا فراخوانی درست انجام شود
 
 # Create your models here.
 
@@ -16,6 +17,60 @@ class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
+    class Meta:
+        # این جادوی اصلی است!
+        # می‌گوید ترکیب این دو فیلد باید در کل دیتابیس یکتا باشد.
+        constraints = [
+            models.UniqueConstraint(
+                fields=['first_name', 'last_name'],
+                name='unique_author_full_name'
+            )
+        ]
+
+    def clean(self):
+        # اعمال همان منطق تمیزکاری که یاد گرفتی
+        if self.first_name:
+            self.first_name = self.first_name.strip()
+        if self.last_name:
+            self.last_name = self.last_name.strip()
+
+    def save(self, *args, **kwargs):
+        # تضمین نهایی برای تمیز بودن داده‌ها
+        if self.first_name:
+            self.first_name = self.first_name.strip()
+        if self.last_name:
+            self.last_name = self.last_name.strip()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Category(models.Model):
 
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True,error_messages={
+            'unique': 'این دسته‌بندی قبلاً وجود دارد' # این پیامِ استانداردِ جنگو را فارسی می‌کند
+        })
+
+    def clean(self):
+        """
+        پاکسازی داده قبل از اعتبارسنجی (مناسب برای فرم‌ها و پنل ادمین)
+        """
+        if self.title:
+            self.title = self.title.strip()
+
+    def save(self, *args, **kwargs):
+        """
+        ضمانت نهایی پاکسازی (مناسب برای همه حالات ذخیره‌سازی)
+        """
+        if self.title:
+            self.title = self.title.strip()
+        super().save(*args, **kwargs)
+
+
+    # def clean(self):
+    #     if Category.objects.filter(title__iexact=self.title).exclude(pk=self.pk).exists():
+    #         raise ValidationError("خطا: این دسته بندی قبلاً وجود دارد!")
+    # in zamani be dard mikhore ke uniqe=true is not defined !!!!!!!!!!!
+
+    def __str__(self):
+        return f"{self.title}"
